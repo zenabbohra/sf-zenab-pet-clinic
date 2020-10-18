a955b90cf8a1d816241cce02df76dba03a9cd946
Zenab Bohra <zenab.bohra19@gmail.com>
2020-10-18 23:11:10 +1100

    Add dependencies

---
+++ src/main/java/com/springframework/sfzenabpetclinic/SfZenabPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.sfzenabpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfZenabPetClinicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SfZenabPetClinicApplication.class, args);
+  }
+}
---
+++ src/test/java/com/springframework/sfzenabpetclinic/SfZenabPetClinicApplicationTests.java
@@ -0,0 +1,12 @@
+package com.springframework.sfzenabpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfZenabPetClinicApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
