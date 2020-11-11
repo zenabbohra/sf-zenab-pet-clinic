2b8cefab84e688611c61fe3401747a79953fcb0e
Zenab Bohra <zenab.bohra19@gmail.com>
2020-11-12 07:50:07 +1100

    Autogenerate ids in map service. Closes #22

--- pet-clinic-data/src/main/java/com/springframework/sfzenabpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfzenabpetclinic/services/map/PetServiceMap.java
@@ -18,7 +18,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long>
   }
 
   @Override public Pet save(Pet object) {
-    return super.save(object.getId(), object);
+    return super.save(object);
   }
 
   @Override public void delete(Pet object) {
